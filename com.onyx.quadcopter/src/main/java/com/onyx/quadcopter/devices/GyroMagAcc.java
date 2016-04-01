c03c312da2f93a55ad50dbdba0be1daa37a77e7c
Alfred Laderoute <alfred_e@outlook.com>
2016-04-01 18:17:59 -0400

    Added evdev-java code

--- com.onyx.quadcopter/src/main/java/com/onyx/quadcopter/devices/GyroMagAcc.java
+++ com.onyx.quadcopter/src/main/java/com/onyx/quadcopter/devices/GyroMagAcc.java
@@ -1,28 +1,27 @@
 package com.onyx.quadcopter.devices;
 
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.onyx.quadcopter.evdev.EventDevice;
+import com.onyx.quadcopter.evdev.InputEvent;
+import com.onyx.quadcopter.evdev.InputListener;
 import com.onyx.quadcopter.main.Controller;
 import com.onyx.quadcopter.messaging.ACLMessage;
 import com.onyx.quadcopter.messaging.MessageType;
 import com.onyx.quadcopter.utils.Constants;
 
-public class GyroMagAcc extends Device {
+public class GyroMagAcc extends Device implements InputListener {
 
     /**
      * Input Device streams.
      */
-    private DataInputStream gyroStream;
-    private DataInputStream accelStream;
-    private DataInputStream magStream;
+    private EventDevice gyroDev;
+    private EventDevice accelDev;
+    private EventDevice magDev;
 
-    private final byte[] timeval = new byte[24];
     private short gyrX, gyrY, gyrZ;
     private short accX, accY, accZ;
     private short magX, magY, magZ;
@@ -41,7 +40,6 @@ public class GyroMagAcc extends Device {
             switch (lastMessage.getActionID()) {
             case GET_ORIENT:
             case SEND_DATA:
-                pollXYZ();
                 final ACLMessage m = new ACLMessage(MessageType.SEND);
                 m.setActionID(lastMessage.getActionID());
                 m.setReciever(lastMessage.getSender());
@@ -55,65 +53,29 @@ public class GyroMagAcc extends Device {
         }
     }
 
-    /**
-     * Get a snapshot of the X Y and Z data.
-     */
-    private synchronized void pollXYZ() {
-        try {
-            gyroStream.read(timeval);
-            gyroStream.readShort();
-            gyroStream.readShort();
-            gyrX = (short) gyroStream.readInt();
-            gyroStream.readShort();
-            gyroStream.readShort();
-            gyrY = (short) gyroStream.readInt();
-            gyroStream.readShort();
-            gyroStream.readShort();
-            gyrZ = (short) gyroStream.readInt();
-
-            accelStream.read(timeval);
-            accelStream.readShort();
-            accelStream.readShort();
-            accX = (short) accelStream.readInt();
-            accelStream.readShort();
-            accelStream.readShort();
-            accY = (short) accelStream.readInt();
-            accelStream.readShort();
-            accelStream.readShort();
-            accZ = (short) accelStream.readInt();
-
-            magStream.read(timeval);
-            magStream.readShort();
-            magStream.readShort();
-            magX = (short) magStream.readInt();
-            magStream.readShort();
-            magStream.readShort();
-            magY = (short) magStream.readInt();
-            magStream.readShort();
-            magStream.readShort();
-            magZ = (short) magStream.readInt();
-        } catch (final IOException e) {
-            LOGGER.error(e.getMessage());
-        }
-    }
-
     @Override
     protected void init() {
         LOGGER.debug("Initializing Gyro, Magnetometer and Accelerometer Device.");
         if (Constants.SIMULATION) {
             try {
-                gyroStream = new DataInputStream(new FileInputStream(Constants.URANDOM));
-                accelStream = new DataInputStream(new FileInputStream(Constants.URANDOM));
-                magStream = new DataInputStream(new FileInputStream(Constants.URANDOM));
-            } catch (final FileNotFoundException e) {
+                gyroDev = new EventDevice(Constants.GYRO_DEV_FILE);
+                gyroDev.addListener(this);
+                accelDev = new EventDevice(Constants.GYRO_DEV_FILE);
+                accelDev.addListener(this);
+                magDev = new EventDevice(Constants.GYRO_DEV_FILE);
+                magDev.addListener(this);
+            } catch (final IOException e) {
                 LOGGER.error(e.getMessage());
             }
         } else {
             try {
-                gyroStream = new DataInputStream(new FileInputStream(Constants.GYRO_DEV_FILE));
-                accelStream = new DataInputStream(new FileInputStream(Constants.ACC_DEV_FILE));
-                magStream = new DataInputStream(new FileInputStream(Constants.MAG_DEV_FILE));
-            } catch (final FileNotFoundException e) {
+                gyroDev = new EventDevice(Constants.URANDOM);
+                gyroDev.addListener(this);
+                accelDev = new EventDevice(Constants.URANDOM);
+                accelDev.addListener(this);
+                magDev = new EventDevice(Constants.URANDOM);
+                magDev.addListener(this);
+            } catch (final IOException e) {
                 LOGGER.error(e.getMessage());
             }
         }
@@ -123,18 +85,13 @@ public class GyroMagAcc extends Device {
     @Override
     public void shutdown() {
         LOGGER.debug("Shutting down Gyro, Magnetometer and Accelerometer Device.");
-        try {
-            gyroStream.close();
-            accelStream.close();
-            magStream.close();
-        } catch (final IOException e) {
-            LOGGER.error(e.getMessage());
-        }
+        gyroDev.close();
+        accelDev.close();
+        magDev.close();
     }
 
     @Override
     protected void alternate() {
-        pollXYZ();
         LOGGER.debug(gyrX + ":" + gyrY + ":" + gyrZ + ";" + accX + ":" + accY + ":" + accZ + ";" + magX + ":" + magY
                 + ":" + magZ);
     }
@@ -142,6 +99,21 @@ public class GyroMagAcc extends Device {
     @Override
     public boolean selfTest() {
         return true;// TODO Complete Gyro self test
+    }
+
+    @Override
+    public void event(final InputEvent e) {
+        switch (e.type) {
+        case InputEvent.ABS_X:
+            gyrX = (short) e.value;
+            break;
+        case InputEvent.ABS_Y:
+            gyrY = (short) e.value;
+            break;
+        case InputEvent.ABS_Z:
+            gyrZ = (short) e.value;
+            break;
+        }
     }
 
 }
