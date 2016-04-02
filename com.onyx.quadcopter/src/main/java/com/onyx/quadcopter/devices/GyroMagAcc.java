4e4dab9e2388c415183943dd7a340f7954a2d3fd
Alfred Laderoute <alfred_e@outlook.com>
2016-04-02 00:26:54 -0400

    Added Intel UPM and MRAA IoT device driver code

--- com.onyx.quadcopter/src/main/java/com/onyx/quadcopter/devices/GyroMagAcc.java
+++ com.onyx.quadcopter/src/main/java/com/onyx/quadcopter/devices/GyroMagAcc.java
@@ -1,27 +1,18 @@
 package com.onyx.quadcopter.devices;
 
-import java.io.IOException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.onyx.quadcopter.evdev.EventDevice;
-import com.onyx.quadcopter.evdev.InputEvent;
-import com.onyx.quadcopter.evdev.InputListener;
 import com.onyx.quadcopter.main.Controller;
 import com.onyx.quadcopter.messaging.ACLMessage;
 import com.onyx.quadcopter.messaging.MessageType;
 import com.onyx.quadcopter.utils.Constants;
 
-public class GyroMagAcc extends Device implements InputListener {
+import upm_lsm9ds0.LSM9DS0;
 
-    /**
-     * Input Device streams.
-     */
-    private EventDevice gyroDev;
-    private EventDevice accelDev;
-    private EventDevice magDev;
+public class GyroMagAcc extends Device {
 
+    private LSM9DS0 lsm;
     private short gyrX, gyrY, gyrZ;
     private short accX, accY, accZ;
     private short magX, magY, magZ;
@@ -36,6 +27,7 @@ public class GyroMagAcc extends Device implements InputListener {
 
     @Override
     protected void update() {
+        lsm.update();
         if (isNewMessage()) {
             switch (lastMessage.getActionID()) {
             case GET_ORIENT:
@@ -44,8 +36,8 @@ public class GyroMagAcc extends Device implements InputListener {
                 m.setActionID(lastMessage.getActionID());
                 m.setReciever(lastMessage.getSender());
                 m.setSender(getId());
-                m.setContent(gyrX + ":" + gyrY + ":" + gyrZ + ";" + accX + ":" + accY + ":" + accZ + ";" + magX + ":"
-                        + magY + ":" + magZ);
+                m.setContent(lsm.getAccelerometer() + ";" + lsm.getGyroscope() + ";" + lsm.getMagnetometer());
+                m.setValue(lsm.getTemperature());
                 getController().getBlackboard().addMessage(m);
             default:
                 break;
@@ -57,27 +49,10 @@ public class GyroMagAcc extends Device implements InputListener {
     protected void init() {
         LOGGER.debug("Initializing Gyro, Magnetometer and Accelerometer Device.");
         if (Constants.SIMULATION) {
-            try {
-                gyroDev = new EventDevice(Constants.GYRO_DEV_FILE);
-                gyroDev.addListener(this);
-                accelDev = new EventDevice(Constants.GYRO_DEV_FILE);
-                accelDev.addListener(this);
-                magDev = new EventDevice(Constants.GYRO_DEV_FILE);
-                magDev.addListener(this);
-            } catch (final IOException e) {
-                LOGGER.error(e.getMessage());
-            }
+            lsm = null;
         } else {
-            try {
-                gyroDev = new EventDevice(Constants.URANDOM);
-                gyroDev.addListener(this);
-                accelDev = new EventDevice(Constants.URANDOM);
-                accelDev.addListener(this);
-                magDev = new EventDevice(Constants.URANDOM);
-                magDev.addListener(this);
-            } catch (final IOException e) {
-                LOGGER.error(e.getMessage());
-            }
+            lsm = new LSM9DS0();
+            lsm.enableTemperatureSensor(true);
         }
 
     }
@@ -85,9 +60,7 @@ public class GyroMagAcc extends Device implements InputListener {
     @Override
     public void shutdown() {
         LOGGER.debug("Shutting down Gyro, Magnetometer and Accelerometer Device.");
-        gyroDev.close();
-        accelDev.close();
-        magDev.close();
+        lsm.delete();
     }
 
     @Override
@@ -99,21 +72,6 @@ public class GyroMagAcc extends Device implements InputListener {
     @Override
     public boolean selfTest() {
         return true;// TODO Complete Gyro self test
-    }
-
-    @Override
-    public void event(final InputEvent e) {
-        switch (e.type) {
-        case InputEvent.ABS_X:
-            gyrX = (short) e.value;
-            break;
-        case InputEvent.ABS_Y:
-            gyrY = (short) e.value;
-            break;
-        case InputEvent.ABS_Z:
-            gyrZ = (short) e.value;
-            break;
-        }
     }
 
 }
