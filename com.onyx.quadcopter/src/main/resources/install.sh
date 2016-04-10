6e5dbf382dd22ca27ee8b4bb21b3583aca2865bb
Alfred Laderoute <alfred_e@outlook.com>
2016-04-09 23:09:55 -0400

    Update install.sh

--- com.onyx.quadcopter/src/main/resources/install.sh
+++ com.onyx.quadcopter/src/main/resources/install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-sudo cp ./systemd/onyx-* /lib/systemd/system/
-for i in ./systemd/onyx-*;
+sudo cp /opt/onyx/systemd/onyx-* /lib/systemd/system/
+for i in /opt/onyx/systemd/onyx-*;
 do
     FILENAME=`basename $i`
     sudo ln -s /lib/systemd/system/$FILENAME  /etc/systemd/system/$FILENAME
 done
+sudo chmod +x /opt/onyx/onyx-*
