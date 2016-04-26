7741efd656db609f08a91df105c2f8494d381d51
Alfred Laderoute <alfred_e@outlook.com>
2016-04-26 19:28:49 -0400

    Adding new GPS code.

--- com.onyx.quadcopter/src/main/java/com/onyx/quadcopter/devices/GpsDevice.java
+++ com.onyx.quadcopter/src/main/java/com/onyx/quadcopter/devices/GpsDevice.java
@@ -1,22 +1,18 @@
 package com.onyx.quadcopter.devices;
 
-
 import com.onyx.common.messaging.AclMessage;
 import com.onyx.common.messaging.ActionId;
 import com.onyx.common.messaging.DeviceId;
 import com.onyx.quadcopter.utils.GpsProcessor;
 
-import net.sf.marineapi.nmea.event.SentenceEvent;
-import net.sf.marineapi.nmea.event.SentenceListener;
+import net.sf.marineapi.nmea.event.AbstractSentenceListener;
 import net.sf.marineapi.nmea.io.SentenceReader;
 import net.sf.marineapi.nmea.sentence.GGASentence;
-import net.sf.marineapi.nmea.sentence.Sentence;
-import net.sf.marineapi.nmea.sentence.SentenceId;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class GpsDevice extends Device implements SentenceListener {
+public class GpsDevice extends Device {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(GpsDevice.class);
   private GGASentence lastSent;
@@ -48,7 +44,7 @@ public class GpsDevice extends Device implements SentenceListener {
     gps = new GpsProcessor();
     gps.start();
     final SentenceReader reader = new SentenceReader(gps.getInputStream());
-    reader.addSentenceListener(this);
+    reader.addSentenceListener(new GgaListener());
     reader.start();
   }
 
@@ -67,32 +63,17 @@ public class GpsDevice extends Device implements SentenceListener {
   @Override
   public boolean selfTest() {
     return true;
-  }
+  }  
 
-  @Override
-  public void readingPaused() {    
-  }
-
-  @Override
-  public void readingStarted() {    
-  }
-
-  @Override
-  public void readingStopped() {    
-  }
-
-  @Override
-  public void sentenceRead(SentenceEvent event) {
-    Sentence ss = event.getSentence();
-    GGASentence ggaSent = null;
-    if (ss.getSentenceId().matches("GGA")) {
-      ggaSent = (GGASentence) ss;
-    }
-    if (ggaSent != null && ggaSent.isValid()) {
-      LOGGER.trace("GGA position: " + ggaSent.getPosition());
-      lastSent = ggaSent;
-    } else {
-      LOGGER.error("Invalid NMEA sentence.");
+  private class GgaListener extends AbstractSentenceListener<GGASentence> {
+    @Override
+    public void sentenceRead(GGASentence sentence) {
+      if (sentence != null && sentence.isValid()) {
+        LOGGER.trace("GGA position: " + sentence.getPosition());
+        lastSent = sentence;
+      } else {
+        LOGGER.error("Invalid NMEA sentence.");
+      }
     }
   }
 
