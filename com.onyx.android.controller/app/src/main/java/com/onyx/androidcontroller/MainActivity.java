6653ca7fb35525a4c4414654ceb2977b5e111bd3
alfred <fredladeroute@gmail.com>
2016-12-24 15:02:21 -0500

    Added connect button code.

--- com.onyx.android.controller/app/src/main/java/com/onyx/androidcontroller/MainActivity.java
+++ com.onyx.android.controller/app/src/main/java/com/onyx/androidcontroller/MainActivity.java
@@ -1,7 +1,14 @@
 package com.onyx.androidcontroller;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.UiThread;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupOnEditTextEvent();
+
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    @UiThread
+    public void connect(View v) {
+        Log.w(getString(R.string.app_name), "Attempting to connect to Quadcopter. " + v.toString());
+    }
+
+    /**
+     * Setup the OnEditText Event.
+     */
+    private void setupOnEditTextEvent() {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    handled = true;
+                    connect(v);
+                }
+                return handled;
+            }
+        });
     }
 }
